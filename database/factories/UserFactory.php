ba2c6275f2e1876b76f73f7f50a4dbdbbea2fb0e
berkmahim <berkmahim620@gmail.com>
2022-03-18 17:33:43 +0300

    layout-jetstream-bootstrap

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        User::insert([]);
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
